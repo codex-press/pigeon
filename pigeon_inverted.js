c90c8520c0a3d2721f1a37224663d7260b7ba8b9
Tim Farnam <twfarnam@gmail.com>
2016-08-23 17:36:11 -0400

    set up structure for less. everything broken (sorry)

---
+++ pigeon_inverted.js
@@ -0,0 +1,33 @@
+// these fonts are for pigeon theme which is also index.less
+ 
+// Adobe Text Pro
+
+(function(d) {
+
+  var config = {
+    kitId: 'qvd6gue',
+    scriptTimeout: 3000,
+    async: true
+  },
+
+  h=d.documentElement,
+  t=setTimeout(function(){
+    h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},
+  config.scriptTimeout),
+  tk=d.createElement("script"),
+  f=false,
+  s=d.getElementsByTagName("script")[0],a;
+  h.className+=" wf-loading";
+  tk.src='https://use.typekit.net/'+config.kitId+'.js';
+  tk.async=true;
+  tk.onload=tk.onreadystatechange=function(){
+    a=this.readyState;
+    if(f||a&&a!="complete"&&a!="loaded")
+      return;
+    f=true;
+    clearTimeout(t);
+    try{Typekit.load(config)}
+    catch(e){}};
+  s.parentNode.insertBefore(tk,s)
+})(document);
+
